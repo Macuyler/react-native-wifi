b22783c04f23927ea095998dc10a401b5ca628aa
Macuyler <macuyler@gmail.com>
2019-11-30 18:10:09 -0600

    Fix dependency issue

--- ios/RNWifi.h
+++ ios/RNWifi.h
@@ -1,11 +1,4 @@
-// Created by Rutger Bresjer on 10/10/2017
-
-// Notes:
-// - Be sure to enable "Hotspot Configuration" capability for the iOS target
-// - Make sure the NetworkExtension framework is linked to the target
-
-#import <Foundation/Foundation.h>
-#import <RCTBridgeModule.h>
+#import <React/RCTBridgeModule.h>
 
 @interface WifiManager : NSObject <RCTBridgeModule>
 
